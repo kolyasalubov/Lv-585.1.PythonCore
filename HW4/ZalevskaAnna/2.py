19927158c8d4c9836e82c72187720c2fcdb4679d
Anna Zalevska <annazalevzka@gmail.com>
2021-03-28 16:41:41 +0300

    Added Homework 4

---
+++ HW4/ZalevskaAnna/2.py
@@ -0,0 +1,6 @@
+a = [1, 8, 9, 10]
+
+for i in range(0, 4):
+    a[i] = float(a[i])
+print(a)
+
---
+++ HW4/ZalevskaAnna/TimeToPrc.1.py
@@ -0,0 +1,9 @@
+a = 6
+b = 8
+if a == b:
+    print("a = b")
+elif a > b:
+    print("a > b")
+elif a < b:
+    print("a < b")
+
