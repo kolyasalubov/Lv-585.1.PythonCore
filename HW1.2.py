f98b45cf84fa6fc32b640b89fded8684bd90ffc1
TarasFreeman <berchuku@gmail.com>
2021-03-29 11:40:14 +0200

    added homework

---
+++ HW1.2.py
@@ -0,0 +1,8 @@
+
+name = input ("What is your name?: ")
+birth_year = int(input("What year where you born?: " ))
+print(type(birth_year))
+current_year = int(2021)
+place = input("where do you live?: ")
+age = current_year - birth_year
+print("Hellow " + name  + "your age is " + age + "you live in " + place)
