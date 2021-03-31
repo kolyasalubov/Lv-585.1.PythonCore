014d25e3db2d092a5907987513427217d94eef1a
TarasFreeman <80819979+TarasFreeman@users.noreply.github.com>
2021-03-31 22:17:03 +0200

    Add files via upload

---
+++ HW4.3.py
@@ -0,0 +1,9 @@
+n1 = 0
+n2 = 1
+i = 0
+while i<10: # we establish how many times this operation will repeat itself due to i + 1 in the end of cycle
+    print(n1)
+    n3 = n1+n2
+    n1 = n2
+    n2 = n3
+    i += 1
