8002e8b75be293e8f05ac270d563e511f4635964
Mari0312 <80839792+Mari0312@users.noreply.github.com>
2021-04-12 21:56:35 +0300

    Update Will there be enough space.py

--- HW5/chyhut/Will there be enough space.py
+++ HW5/chyhut/Will there be enough space.py
@@ -1,7 +1,6 @@
 def enough(cap, on, wait):
-    x = cap - on 
-    if(x < wait):
-      return(wait - x)
-    else: 
-        return(0) 
-
+    x = cap - on
+    if x < wait:
+        return wait - x
+    else:
+        return 0
