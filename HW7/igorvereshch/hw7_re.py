8e14147b519ba6cf1dd959dc2fe6987659b75de4
Igor Vereshchynskyi <igorvereshch@ukr.net>
2021-04-14 14:42:04 +0300

    edited homework7_task3.py

--- HW7/igorvereshch/hw7_re.py
+++ HW7/igorvereshch/hw7_re.py
@@ -4,18 +4,4 @@ def is_valid(password):
     pattern = r"(?=.*[$#@])(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=^.{6,16}$)"
     if re.search(pattern, password):
         return 'valid'
-    else:
-        return 'invalid'
-
-# Just for tests
-passwords = []
-passwords.append("Ab3#") # Invalid because too short (4 symbols)
-passwords.append("Abcd5fghi0@#$@#$##!z") # Invalid because too long (20 symbols)
-passwords.append("abc3def#") # Invalid because no capital letter
-passwords.append("ABC3DEF#") # Invalid because no lowercase letter
-passwords.append("Abcdef7") # Invalid because no symbol $, #, or @
-passwords.append("Abcdef#") # Invalid because no digits
-passwords.append("Abc def3#!") # Valid, no limits on ' ' or '!' symbols are given
-passwords.append("Abc3def#") # Valid, normal password
-for p in passwords:
-    print(f"{p} is {is_valid(p)}")
+    return 'invalid'
