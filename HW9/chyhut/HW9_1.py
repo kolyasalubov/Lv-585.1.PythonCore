01c09c812019d13e0db5269b58e9d13d72288a37
Mariia <chyhut@gmail.com>
2021-04-21 21:21:02 +0300

    Revert "added HW9"
    This reverts commit a631c4009f1a79ad4ec6e4ee8b7d66d9b49d8cc2.

--- HW9/chyhut/HW9_1.py
+++
@@ -1,8 +0,0 @@
-def age_person(age):
-    age = input("Enter your age: ")
-    if age < 0:
-        raise ValueError("Only positive number")
-    if age % 2 == 0:
-        print("Your {age} is even")
-    else:
-        print("Your {age} is odd")
