e819c7affd055bca82b2380652af96038b605f03
Anna Zalevska <annazalevzka@gmail.com>
2021-04-29 11:43:22 +0300

    Added Project: updated

--- Project/ZalevskaAnna/Project.py/Project.py
+++ Project/ZalevskaAnna/Project.py/Project.py
@@ -1,15 +1,15 @@
-import Liner_Calc
 def clc():
-    ch = input("1 - Ordinary calculator, 2 - linear calculator, 3 - Quadratic calculator, 4 - Draw a graph: ")
-    if ch == '1':
-        import Calc
-    elif ch == '2':
-        return Liner_Calc.calc()
-    elif ch == '3':
-        import Quadratic_calculator
-    elif ch == '4':
-        import Graf
-    else:
-        return "Input error"
+    try:
+        ch = input("1 - Ordinary calculator, 2 - Quadratic calculator, 3 - Draw a graph: ")
+        if ch == '1':
+            import Calc
+        elif ch == '2':
+            import Quadratic_calculator
+        elif ch == '3':
+            import Graf
+        else:
+            raise ValueError("You must enter a number in the range 1-3")
+    except ValueError as e: 
+        print(e)
 print (clc())
 
--- Project/ZalevskaAnna/Project.py/Quadratic_calculator.py
+++ Project/ZalevskaAnna/Project.py/Quadratic_calculator.py
@@ -1,48 +1,47 @@
 from math import sqrt
 from tkinter import *
-def calc():
-    def solver(a,b,c):
-        D = b*b - 4*a*c
-        if D >= 0:
-            x1 = (-b + sqrt(D)) / (2*a)
-            x2 = (-b - sqrt(D)) / (2*a)
-            text = "The discriminant is: %s \n X1 is: %s \n X2 is: %s \n" % (D, x1, x2)        
-        else:
-            text = "The discriminant is: %s \n This equation has no solutions" % D 
-        return text
-    root = Tk()
-    root.title("Quadratic calculator") 
-    root.minsize(325,230)
-    root.resizable(width=False, height=False)
-    
-    frame = Frame(root)
-    frame.grid()
 
-    def inserter(value):
-        """ Inserts specified value into text widget """
-        output.delete("0.0","end")
-        output.insert("0.0",value)
-    def handler():
-        """ Get the content of entries and passes result to the output area """
-        try:
-            a_val = float(a.get())
-            b_val = float(b.get())
-            c_val = float(c.get())
-            inserter(solver(a_val, b_val, c_val))
-        except ValueError:
-            inserter("Make sure you entered 3 numbers")
+def solver(a,b,c):
+    D = b*b - 4*a*c
+    if D >= 0:
+        x1 = (-b + sqrt(D)) / (2*a)
+        x2 = (-b - sqrt(D)) / (2*a)
+        text = "The discriminant is: %s \n X1 is: %s \n X2 is: %s \n" % (D, x1, x2)        
+    else:
+        text = "The discriminant is: %s \n This equation has no solutions" % D 
+    return text
+root = Tk()
+root.title("Quadratic calculator") 
+root.minsize(325,230)
+root.resizable(width=False, height=False)
     
-    a = Entry(frame, width=3)
-    a.grid(row=1,column=1,padx=(10,0))
-    a_lab = Label(frame, text="x**2+").grid(row=1,column=2)
-    b = Entry(frame, width=3)
-    b.grid(row=1,column=3)
-    b_lab = Label(frame, text="x+").grid(row=1, column=4)
-    c = Entry(frame, width=3)
-    c.grid(row=1, column=5)
-    c_lab = Label(frame, text="= 0").grid(row=1, column=6)
-    but = Button(frame, text="Solve", command = handler).grid(row=1, column=7, padx=(10,0)) 
-    output = Text(frame, bg="lightblue", font="Arial 12", width=35, height=10)
-    output.grid(row=2, columnspan=8)
-    root.mainloop()
-print(calc())
+frame = Frame(root)
+frame.grid()
+
+def inserter(value):
+    """ Inserts specified value into text widget """
+    output.delete("0.0","end")
+    output.insert("0.0",value)
+def handler():
+    """ Get the content of entries and passes result to the output area """
+    try:
+        a_val = float(a.get())
+        b_val = float(b.get())
+        c_val = float(c.get())
+        inserter(solver(a_val, b_val, c_val))
+    except ValueError:
+        inserter("Make sure you entered 3 numbers")
+    
+a = Entry(frame, width=3)
+a.grid(row=1,column=1,padx=(10,0))
+a_lab = Label(frame, text="x**2+").grid(row=1,column=2)
+b = Entry(frame, width=3)
+b.grid(row=1,column=3)
+b_lab = Label(frame, text="x+").grid(row=1, column=4)
+c = Entry(frame, width=3)
+c.grid(row=1, column=5)
+c_lab = Label(frame, text="= 0").grid(row=1, column=6)
+but = Button(frame, text="Solve", command = handler).grid(row=1, column=7, padx=(10,0)) 
+output = Text(frame, bg="lightblue", font="Arial 12", width=35, height=10)
+output.grid(row=2, columnspan=8)
+root.mainloop()
