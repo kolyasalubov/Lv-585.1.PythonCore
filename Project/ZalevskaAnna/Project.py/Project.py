280459d6c40c7a1da0ddaaaa4e947a3bb03be016
Anna Zalevska <annazalevzka@gmail.com>
2021-04-29 13:11:09 +0300

    Poject: Apdate

--- Project/ZalevskaAnna/Project.py/Project.py
+++ Project/ZalevskaAnna/Project.py/Project.py
@@ -1,15 +1,22 @@
 def clc():
-    try:
-        ch = input("1 - Ordinary calculator, 2 - Quadratic calculator, 3 - Draw a graph: ")
-        if ch == '1':
-            import Calc
-        elif ch == '2':
-            import Quadratic_calculator
-        elif ch == '3':
-            import Graf
-        else:
-            raise ValueError("You must enter a number in the range 1-3")
-    except ValueError as e: 
-        print(e)
+    ch = input("1 - Ordinary calculator, 2 - Quadratic calculator, 3 - Draw a graph, 0 - Exit: ")
+    while True:
+        try:
+            if ch == '1':
+                import Calc
+                ch = input("1 - Ordinary calculator, 2 - Quadratic calculator, 3 - Draw a graph, 0 - Exit: ")
+            elif ch == '2':
+                import Quadratic_calculator
+                ch = input("1 - Ordinary calculator, 2 - Quadratic calculator, 3 - Draw a graph, 0 - Exit: ")
+            elif ch == '3':
+                import Graf
+                ch = input("1 - Ordinary calculator, 2 - Quadratic calculator, 3 - Draw a graph, 0 - Exit: ")
+            elif ch == '0':
+                break
+            else:
+                raise ValueError("You must enter a number in the range 1-3")
+        except ValueError as e: 
+            print(e)
+            ch = input("1 - Ordinary calculator, 2 - Quadratic calculator, 3 - Draw a graph, 0 - Exit: ")
 print (clc())
 
